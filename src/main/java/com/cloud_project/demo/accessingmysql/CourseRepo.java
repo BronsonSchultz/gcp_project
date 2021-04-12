169bb9da625f78de790357d4ba47d6918f6b6090
bronson <bronson.schultz1@gmail.com>
2021-04-11 23:13:07 -0600

    rebase

--- src/main/java/com/cloud_project/demo/accessingmysql/CourseRepo.java
+++ src/main/java/com/cloud_project/demo/accessingmysql/CourseRepo.java
@@ -2,8 +2,6 @@ package com.cloud_project.demo.accessingmysql;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.cloud_project.demo.accessingmysql.Course;
-
 public interface CourseRepo extends CrudRepository<Course, Integer>{
 
 }
